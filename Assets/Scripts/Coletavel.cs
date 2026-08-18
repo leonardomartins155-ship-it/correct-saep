134f3d261a6b14d6de919dd9b5a1dda2f76f025a
leonardomartins155-ship-it <leonardo_martins155@estudante.sesisenai.org.br>
2026-08-18 17:10:06 -0300

    add sond effects

--- Assets/Scripts/Coletavel.cs
+++ Assets/Scripts/Coletavel.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Coletavel : MonoBehaviour
 {
     public GameManager GameManager;
+    public AudioClip coletavel;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(coletavel, transform.position);
             GameManager.Cura();
             GameObject.Destroy(gameObject);
         }
--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Obstaculo : MonoBehaviour
 {
     public GameManager GameManager;
+    public AudioClip somDano;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag ("Player"))
         {
+            AudioSource.PlayClipAtPoint(somDano,transform.position);
             GameManager.Dano();
         }
     }
